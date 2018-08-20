c2f57e1dcde6f5cd1a6d595999032e2eb8bbbe1a
kmv50 <kevinmarinv05@gmail.com>
2018-08-19 20:58:20 -0600

    xades

---
+++ DataModel/Conversores/FacturaDB_ToFacturaElectronica.cs
@@ -0,0 +1,24 @@
+﻿using DataModel.EF;
+using FacturaElectronica_V_4_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel.Conversores
+{
+    public class FacturaDB_ToFacturaElectronica
+    {
+        public FacturaElectronica Convertir(Factura facturaDB)
+        {
+            FacturaElectronica facturaHacienda = new FacturaElectronica()
+            {
+
+            };
+
+
+            return facturaHacienda;
+        }
+    }
+}
